b6480cf2a6fc3090c44e500d69dc68e3499c7573
aaronregan <aaronregan20@gmail.com>
2024-08-07 13:09:42 +0100

    Cleanup Spark Jobs

--- src/main/java/org/spark/project/jobs/AverageNumberOfVotesJob.java
+++ src/main/java/org/spark/project/jobs/AverageNumberOfVotesJob.java
@@ -2,6 +2,7 @@ package org.spark.project.jobs;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.functions;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -10,19 +11,9 @@ public class AverageNumberOfVotesJob {
     public double run(Dataset<Row> ratingsDataset) {
 
         // Filter movies with fewer than 500 votes
-        Dataset<Row> filteredDf = ratingsDataset.filter(col("numVotes").geq(500));
-
-        // Calculate the total number of votes and the count of movies
-        Dataset<Row> aggregatedDf = filteredDf.agg(
-                sum("numVotes").alias("totalVotes"),
-                count("numVotes").alias("movieCount")
-        );
-
-        // Collect the results and calculate the average number of votes
-        Row result = aggregatedDf.collectAsList().get(0);
-        long totalVotes = result.getLong(0);
-        long movieCount = result.getLong(1);
-        double averageNumberOfVotes = (double) totalVotes / movieCount;
+        double averageNumberOfVotes = ratingsDataset
+                .filter(col("numVotes").geq(500))
+                .agg(functions.avg("numVotes")).first().getDouble(0);
 
         // Print the result to the console
         System.out.println("Average Number of Votes: " + averageNumberOfVotes);
--- src/test/java/org/spark/project/jobs/Top10RatedMoviesJobTest.java
+++ src/test/java/org/spark/project/jobs/Top10RatedMoviesJobTest.java
@@ -45,11 +45,11 @@ public class Top10RatedMoviesJobTest {
         );
 
         List<Row> movies = Arrays.asList(
-                RowFactory.create("tt0000001", "Movie", "The"),
-                RowFactory.create("tt0000002", "Movie", "hello"),
-                RowFactory.create("tt0000003", "Movie", "World"),
-                RowFactory.create("tt0000004", "Movie", "Test"),
-                RowFactory.create("tt0000005", "Movie", "Unit")
+                RowFactory.create("tt0000001", "movie", "The"),
+                RowFactory.create("tt0000002", "movie", "hello"),
+                RowFactory.create("tt0000003", "movie", "World"),
+                RowFactory.create("tt0000004", "movie", "Test"),
+                RowFactory.create("tt0000005", "movie", "Unit")
 
                 );
 
@@ -75,6 +75,52 @@ public class Top10RatedMoviesJobTest {
         assertEquals("tt0000005", result.get(1).getString(0));
         assertEquals("tt0000001", result.get(2).getString(0));
         assertEquals("tt0000004", result.get(3).getString(0));
+
+        // Validate rankingScore for the top result (tt0000005)
+        assertEquals(13.6875, result.get(0).getDouble(2), 0.01);
+    }
+
+    @Test
+    public void testFiltersOutNonMoviesCorrectly() {
+        // Sample data
+        List<Row> data = Arrays.asList(
+                RowFactory.create("tt0000001", 5.7, 1000),
+                RowFactory.create("tt0000002", 7.3, 1500),
+                RowFactory.create("tt0000003", 8.0, 300),
+                RowFactory.create("tt0000004", 6.5, 700),
+                RowFactory.create("tt0000005", 9.0, 1000)
+        );
+
+        List<Row> movies = Arrays.asList(
+                RowFactory.create("tt0000001", "movie", "The"),
+                RowFactory.create("tt0000002", "movie", "hello"),
+                RowFactory.create("tt0000003", "movie", "World"),
+                RowFactory.create("tt0000004", "movie", "Test"),
+                RowFactory.create("tt0000005", "tv show", "Unit")
+
+        );
+
+        // Create a DataFrame
+        Dataset<Row> ratingsDataset = spark.createDataFrame(data, SchemaLoader.getRatingSchema());
+        Dataset<Row> moviesDataset = spark.createDataFrame(movies, SchemaLoader.getMovieSchema());
+
+        // Define the average number of votes
+        double averageNumberOfVotes = 800.0;
+
+        // Run the job
+        Top10RatedMoviesJob job = new Top10RatedMoviesJob();
+        Dataset<Row> top10Movies = job.run(ratingsDataset, moviesDataset, averageNumberOfVotes);
+
+        // Assert expected number of rows
+        assertEquals(3, top10Movies.count());
+
+        // Convert to local collection for testing
+        List<Row> result = top10Movies.collectAsList();
+
+        // Validate results (tconst and rankingScore)
+        assertEquals("tt0000002", result.get(0).getString(0));
+        assertEquals("tt0000001", result.get(1).getString(0));
+        assertEquals("tt0000004", result.get(2).getString(0));
 
         // Validate rankingScore for the top result (tt0000005)
         assertEquals(13.6875, result.get(0).getDouble(2), 0.01);
